9d0d2b9c857f3dcc306997baae1c1c21ce370512
shyam0205 <vigneshshyam8@gmail.com>
2022-03-13 14:18:57 +0530

    tamilnadu code added

---
+++ src/org/India/tamilNadu.java
@@ -0,0 +1,21 @@
+package org.India;
+
+public class tamilNadu {
+
+	private void tamilNaduchennai() {
+
+		System.out.println("itcompany");
+		
+	}
+	private void tamilNaduvillupram() {
+
+		System.out.println("pollution");
+		
+	}
+	public static void main(String[] args) {
+		tamilNadu shyam=new tamilNadu();
+		shyam.tamilNaduchennai();
+		shyam.tamilNaduvillupram();
+	}
+	}
+
