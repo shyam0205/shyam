9d9f9ea634062f3d847e9cd1a6f19b0b9e749d6c
shyam0205 <vigneshshyam8@gmail.com>
2022-03-15 11:22:35 +0530

    new added file

--- src/org/India/tamilNadu.java
+++ src/org/India/tamilNadu.java
@@ -10,12 +10,14 @@ public class tamilNadu {
 	private void tamilNaduvillupram() {
 
 		System.out.println("pollution");
+		System.out.println("male");
 		
 	}
 	public static void main(String[] args) {
 		tamilNadu shyam=new tamilNadu();
 		shyam.tamilNaduchennai();
 		shyam.tamilNaduvillupram();
+		
 	}
 	}
 
